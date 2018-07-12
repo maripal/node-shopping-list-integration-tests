8ccbfdf8e7a879b8e0948ec0bce23ebd6a6fa607
Maria Palacios <imarip09@gmail.com>
2018-07-12 15:43:28 -0500

    fixed some error

--- test/test-recipes.js
+++ test/test-recipes.js
@@ -66,9 +66,9 @@ describe("Recipes", function () {
         })
         .then(function(res) {
             expect(res).to.have.status(204);
-            expect(res).to.be.json;
-            expect(res.body).to.be.a('object');
-            expect(res.body).to.deep.equal(updateRecipe);
+            //expect(res).to.be.json;
+            //expect(res.body).to.be.a('object');
+            //expect(res.body).to.deep.equal(updateRecipe);
         });
     });
 
